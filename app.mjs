fc7ce23d7a7eba0a3ded637a4fd5a7881c44c952
Nicole Abramowski <nabramowski@articulate.com>
2024-01-04 16:48:40 -0800

    Get audio working and return results in order

--- app.mjs
+++ app.mjs
@@ -1,9 +1,15 @@
-const express = require('express');
-const webcam = require('node-webcam');
-const sharp = require('sharp');
-const path = require('path');
-const fs = require('fs');
-const Replicate = require('replicate');
+import express from "express";
+import fetch from 'node-fetch';
+import webcam from 'node-webcam';
+import sharp from 'sharp';
+import path from 'path';
+import fs from 'fs';
+import Replicate from 'replicate';
+import createPlayer from 'play-sound';
+
+const player = createPlayer({});
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
@@ -15,15 +21,21 @@ const replicate = new Replicate({
 // Serve static files from the 'frames' directory
 app.use('/frames', express.static(path.join(__dirname, 'frames')));
 
-// Set up a route to serve the HTML page
+// Route to serve the HTML page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Set up a route to handle the button click and capture an image
-app.get('/capture', captureAndDisplayImage, getAIImageDescription);
+app.use(express.json());
 
-async function captureAndDisplayImage (req, res, next) {
+// Route to capture image, get description and play audio narration 
+app.get('/capture', captureAndDisplayImage);
+
+app.post('/describe', getAIImageDescription);
+
+app.post('/read', generateAudio);
+
+async function captureAndDisplayImage (req, res) {
     try {
         const captureOptions = {
           width: 640,
@@ -42,6 +54,7 @@ async function captureAndDisplayImage (req, res, next) {
               reject(new Error('error capturing image'));
             } else {
               // Resolve with the image buffer
+              console.log("Image successfully captured...")
               resolve(Buffer.from(data));
             }
           });
@@ -50,13 +63,13 @@ async function captureAndDisplayImage (req, res, next) {
         // Resize the image using sharp
         const resizedImgBuffer = await sharp(imgBuffer).resize(250).toBuffer();
     
-        // Save the resized image to a file (you may want to save it with a unique name)
+        // Save the resized image to a file
         const imagePath = path.join(__dirname, 'frames', 'captured_frame.jpg');
         fs.writeFileSync(imagePath, resizedImgBuffer);
           
         req.imagePath = imagePath;
 
-        return next();
+        return res.status(200).send({ imagePath: path.relative(__dirname, imagePath) })
       } catch (error) {
         console.error('Error:', error);
     
@@ -70,16 +83,18 @@ async function captureAndDisplayImage (req, res, next) {
       }
 }
 
-async function getAIImageDescription (req, res, next) {
+async function getAIImageDescription (req, res) {
     try {
         // Read the file into a buffer
-        const data = await fs.promises.readFile(req.imagePath);
+        const data = await fs.promises.readFile(req.body.imagePath);
         // Convert the buffer into a base64-encoded string
         const base64 = data.toString("base64");
         // Set MIME type for PNG image
         const mimeType = "image/jpeg";
         // Create the data URI
         const dataURI = `data:${mimeType};base64,${base64}`;
+
+        console.log("Getting image description...")
 
         const prediction = await replicate.run(
             "yorickvp/llava-13b:e272157381e2a3bf12df3a8edd1f38d1dbd736bbb7437277c8b34175f8fce358",
@@ -93,6 +108,8 @@ async function getAIImageDescription (req, res, next) {
               }
             },
           );
+          
+          console.log("Narration sent to Sir David Attenborough for review...");
 
           const outputWithTone = await replicate.run(
             "mistralai/mistral-7b-instruct-v0.1:5fe0a3d7ac2852264a25279d1dfb798acbc4d49711d126646594e212cb821749",
@@ -101,7 +118,7 @@ async function getAIImageDescription (req, res, next) {
                 debug: false,
                 top_k: 50,
                 top_p: 0.9,
-                prompt: `Here is a description of an image after the word 'DESCRIPTION. Change the tone of the existing description to sound like Sir David Attenborough narrating a nature documentary. Make it snarky and funny. Return only the narration as a string.
+                prompt: `<s>[INST]Here is a description of an image below.[/INST]<s> [INST]Change the tone of the existing description to sound like Sir David Attenborough narrating a nature documentary.[/INST] [INST]Make it snarky and funny.[/INST] [INST]Return only the narration as a string.[/INST]
                 \n\nDESCRIPTION:\n${prediction.join(' ')}`,
                 temperature: 0.7,
                 max_new_tokens: 128,
@@ -109,10 +126,52 @@ async function getAIImageDescription (req, res, next) {
               }
             }
           );
-        relativePath = path.relative(__dirname, req.imagePath);
 
-        return res.send({text: 'Image captured and resized successfully!', imagePath: relativePath, description: outputWithTone.join(' ')});
+        let description = outputWithTone.join(' ');
 
+        if (description.includes("NARRATION:")) {
+          const parsedResult = description.split('NARRATION:');
+          description = parsedResult[1];
+        }
+
+        if (description.includes("NARRATOR:")) {
+          const parsedResult = description.split('NARRATOR:');
+          description = parsedResult[1];
+        }
+
+        return res.status(200).send({ description });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+async function generateAudio (req, res) {
+    try {
+        const response = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${process.env.ELEVENLABS_VOICE_ID}`, 
+            {
+                method: 'POST',
+                headers: {'xi-api-key': process.env.ELEVENLABS_API_KEY, 'Content-Type': 'application/json'},
+                body: JSON.stringify({ text: req.body.description })
+            }
+        );
+
+        const data = await response.body;
+
+        // Write the buffer to a temporary file
+        const tempFilePath = path.join(__dirname, 'temp', 'temp_audio.mp3');
+        await fs.promises.writeFile(tempFilePath, data);
+
+        // Play the temporary file
+        player.play(tempFilePath, (err) => {
+          if (err) {
+          console.error('Error playing audio:', err);
+          return res.status(500).send('Error playing audio');
+          }
+
+          res.status(200).send("You're officially the star of Planet Earth!")
+        });
+
+ 
     } catch (error) {
         console.error('Error:', error);
     }
